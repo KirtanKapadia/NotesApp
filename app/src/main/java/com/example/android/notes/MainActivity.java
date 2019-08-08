194df030733c51d096ad2456b3d770349dcad883
KirtanKapadia <kirtankapadia13@gmail.com>
2019-08-09 00:48:02 +0530

    layout done

---
+++ app/src/main/java/com/example/android/notes/MainActivity.java
@@ -0,0 +1,76 @@
+package com.example.android.notes;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+import static android.R.layout.simple_list_item_1;
+import static android.R.layout.simple_list_item_2;
+
+public class MainActivity extends AppCompatActivity {
+
+    static ArrayList<String> notes = new ArrayList<>();
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.main_menu, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        super.onOptionsItemSelected(item);
+
+        switch (item.getItemId()){
+            case R.id.addNote:
+                return true;
+            case R.id.help:
+                new AlertDialog.Builder(this)
+                        .setIcon(android.R.drawable.ic_dialog_alert)
+                        .setTitle("Help")
+                        .setMessage("Long Press on a Note to delete them").show();
+                return true;
+            default:
+                return true;
+
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ListView listView = findViewById(R.id.notesListView);
+
+        notes.add("Example Note");
+
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this, simple_list_item_1, notes);
+        listView.setAdapter(arrayAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+                Intent intent = new Intent(getApplicationContext(), NoteEditorActivity.class);
+                intent.putExtra("noteId", i);
+                startActivity(intent);
+
+            }
+
+        });
+    }
+}
---
+++ app/src/main/java/com/example/android/notes/NoteEditorActivity.java
@@ -0,0 +1,45 @@
+package com.example.android.notes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
+
+public class NoteEditorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_note_editor);
+
+
+        EditText editText = findViewById(R.id.notesEditText);
+
+        Intent intent = getIntent();
+        int noteId = intent.getIntExtra("noteId", -1);
+
+        if(noteId != -1){
+            editText.setText(MainActivity.notes.get(noteId));
+        }
+
+        editText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+    }
+}
